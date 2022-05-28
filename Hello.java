471adcbca95e7789dde52710bedc9ef2afecde86
leeyoosong <zaxscd95@naver.com>
2022-05-28 21:22:47 +0900

    ClassLoader_Commit

--- Hello.java
+++ Hello.java
@@ -1,6 +1,17 @@
 public class Hello {
 
+    public static void main(String[] args) {
+
+        System.out.println("I am a classLoader!! I load Classes..");
+
+        /**
+         *  classLoader : 클래스를 로드한다.
+         */
+        ClassLoader loader = new ClassLoader() {
+
+        };
 
 
+    }
 
 }
