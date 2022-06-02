5e899409807ba7251cb49af4c7e643336ef4ec02
leeyoosong <zaxscd95@naver.com>
2022-06-02 21:16:23 +0900

    Animal과 Machine을 라이브러리로 추가

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
 public class Hello {
 
+    /**
+     * Animal.jar와 Machine.jar를 추가하여 빌드 성공
+     * 이 두 가지를 동적으로 로드할 수 있으면 java의 Dynamic classLoading에 대해 이해할 수 있다.
+     * @param args
+     */
     public static void main(String[] args) {
 
         System.out.println("I am a classLoader!! I load Classes..");
