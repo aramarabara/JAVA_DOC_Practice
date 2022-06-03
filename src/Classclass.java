bf18e7e8db218b9a4535071caf556108f2874f27
leeyoosong <zaxscd95@naver.com>
2022-06-04 00:23:21 +0900

    JAR EXPORT_IMPORT 성공

---
+++ src/Classclass.java
@@ -0,0 +1,15 @@
+import Animal.Animal;
+
+public class Classclass {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+
+        Animal animal = new Animal();
+
+        System.out.println(animal.eat());
+
+
+    }
+
+}
