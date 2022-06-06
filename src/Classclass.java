92ad902740c8c500ebdf282872a848eecdc739e3
leeyoosong <zaxscd95@naver.com>
2022-06-06 22:25:15 +0900

    JAR EXPORT_IMPORT 성공2

--- src/Classclass.java
+++ src/Classclass.java
@@ -1,7 +1,11 @@
 import Animal.Animal;
+import Machine.A_Machine;
 
 public class Classclass {
 
+    /**
+     * 기존의 Machine과 Animal을 빌드하여 JAR파일 출력, 외부 라이브러리로 재호출 성공하였다.
+     */
     public static void main(String[] args) {
         System.out.println("hello");
 
@@ -9,6 +13,10 @@ public class Classclass {
 
         System.out.println(animal.eat());
 
+        A_Machine machine = new A_Machine();
+
+        System.out.println(machine.run(true));
+        System.out.println(machine.run(false));
 
     }
 
