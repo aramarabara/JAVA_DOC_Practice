9a1b788c1bb7c5335c6cc46211f6437959a03b54
leeyoosong <zaxscd95@naver.com>
2022-06-17 23:42:36 +0900

    Java Inheritance & MultipleInheritance & Override 학습

---
+++ src/OOP/MultipleInheritance/CarMultipleInheritance.java
@@ -0,0 +1,14 @@
+package OOP.MultipleInheritance;
+
+/*public class CarMultipleInheritance extends Machine, Thing{
+}*/
+/*
+* Java는 C++과 달리 Multiple Inheritance를 지원하지 않는다.
+*
+* diamond problem, deadly diamond of death, 다이아몬드 문제가 발생한다.
+*
+* 출처 : https://zetawiki.com/wiki/%EB%8B%A4%EC%9D%B4%EC%95%84%EB%AA%AC%EB%93%9C_%EB%AC%B8%EC%A0%9C
+*
+*  다중상속 시 둘 이상의 부모클래스가 있고, 같은 메서드를 가질 경우 어떤 것을 상속받아야 하는지 모호한 문제
+*
+* */
---
+++ src/OOP/Overriding/SchoolBus.java
@@ -0,0 +1,13 @@
+package OOP.Overriding;
+
+import OOP.inheritance.Car;
+
+public class SchoolBus extends Car {
+
+    @Override
+    public void drive(String owner) {
+        /* 스쿨버스는 학생들과 함께합니다. */
+        System.out.println("SchoolBus drives with students");
+        super.drive(owner);
+    }
+}
