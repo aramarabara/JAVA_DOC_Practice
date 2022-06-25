009cf0b1b38239adb95873782ce09ba3482dbae9
leeyoosong <zaxscd95@naver.com>
2022-06-25 22:28:10 +0900

    Java Interface & 단일상속 개념 보완

---
+++ src/OOP/Interface/Abstract/Car.java
@@ -0,0 +1,26 @@
+package OOP.Interface.Abstract;
+
+// 상속관계가 항상 완벽하게 준비되어있지는 않다.
+public class Car {
+
+    public String owner;
+    public String angle;
+    public String direction;
+
+    public void drive(String owner) {
+        System.out.println("Car drives with " + owner);
+    }
+
+    public void turn(String direction, String angle) {
+        System.out.println("Car turns to " + direction + " and " + angle + "slightly");
+    }
+
+    public void turnOverride(String direction, String angle) {
+        System.out.println("Car turns to " + direction + " and " + angle + "slightly");
+    }
+
+    public void use(){
+        System.out.println("Car uses");
+    }
+
+}
--- src/OOP/Polymorphism/Main.java
+++ src/OOP/Polymorphism/Main.java
@@ -1,23 +1,32 @@
 package OOP.Polymorphism;
 
+import OOP.Overriding.Ambulance;
+
 import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     // 다형성 (Polymorphism) 실험
     public static void main(String[] args) {
-        Car car = new Car();
 
-        Object object = car;
-        Machine machine = car;
-        Car newCar = car;
+        Car car = new Car(); // 차량을 생성합니다.
+
+        //Map car2 = new Car(); 이렇게 하면 컴파일 오류가 발생합니다.
+        Map car2 = (Map) new Car(); // 이렇게 하면 오류가 발생하지 않습니다.
+
+        Machine car3 = new Car(); // 이렇게 하면 오류가 발생하지 않습니다.
+
+
+        Object object = car; // Object 타입으로 차량을 변환합니다.
+        Machine machine = car; // Machine 타입으로 차량을 변환합니다.
+        Car newCar = car; // Car 타입으로 차량을 변환합니다.
 
         Machine newMachine = new Machine();
-        /*형변환 오류 발생*//*
-        Car polyCar = newMachine;
-        */
-        // 강제 형변환 시에는 runtime 오류가 발생하기 때문에 더 심각하다.
-        Car polyCar = (Car)newMachine;
 
+        //Car polyCar = newMachine; // 오류가 발생합니다.
+        // 강제 형변환 시에는 runtime 오류가 발생하기 때문에 더 심각합니다.
+
+        Car polyCar = (Car)newMachine;
 
         //미리 형변환을 알아보기 위해서 InstanceOf()를 사용한다.
         // InstanceOf는 연산자이므로, 타입과 같이 연산하여
@@ -40,6 +49,7 @@ public class Main {
 
         // 반대는 불가능, 범위가 더 넓기 때문에 강제 형변환 ()을 통해 보내야 한다.
         polyCar.polymorphismInMethod3((Car)machine);
+
 
     }
 
