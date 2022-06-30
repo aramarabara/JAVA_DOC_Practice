3fdf9d51f4d8c766ab1abbf39377f06236846e37
leeyoosong <zaxscd95@naver.com>
2022-06-30 22:06:30 +0900

    Java IO 기본학습3

--- src/JavaIO/PathPractice.java
+++ src/JavaIO/PathPractice.java
@@ -1,6 +1,8 @@
 package JavaIO;
 
+import java.io.IOException;
 import java.net.URI;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -61,7 +63,41 @@ public class PathPractice  {
         // instance returns expected values.
         Path fullPath = inputPath.toAbsolutePath();
 
+        //파일이 없을 경우 예외를 던진다.
+        try {
+            Path fp = path.toRealPath();
+        } catch (NoSuchFileException x) {
+            System.err.format("%s: no such" + " file or directory%n", path);
+            // Logic for case when file doesn't exist.
+        } catch (IOException x) {
+            System.err.format("%s%n", x);
+            // Logic for other sort of file error.
+        }
 
+        Path home = Paths.get("home");
+        Path relativePath = Paths.get("home/sally/bar");
+        // Result is sally/bar
+        Path home_to_relativePath = home.relativize(relativePath);
+        // Result is ../..
+        Path relativePath_to_home = relativePath.relativize(home);
+
+
+
+        Path beginning = Paths.get("/home");
+        Path ending = Paths.get("foo");
+
+        if (path.equals(relativePath)) {
+            // equality logic here
+        } else if (path.startsWith(beginning)) {
+            // path begins with "/home"
+        } else if (path.endsWith(ending)) {
+            // path ends with "foo"
+        }
+
+        // Path는 iterator를 제공한다.
+        for (Path name: path) {
+            System.out.println(name);
+        }
 
     }
 
