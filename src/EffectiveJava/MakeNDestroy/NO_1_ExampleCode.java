a4e39e8310d6ea2fe0ed16307ec79e97e9b8bf21
leeyoosong <zaxscd95@naver.com>
2022-07-09 23:17:59 +0900

    EffectiveJava 실습

--- src/EffectiveJava/MakeNDestroy/NO_1_ExampleCode.java
+++ src/EffectiveJava/MakeNDestroy/NO_1_ExampleCode.java
@@ -1,5 +1,10 @@
 package EffectiveJava.MakeNDestroy;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.EnumSet;
+
 public class NO_1_ExampleCode {
 
     public static void main(String[] args) {
@@ -35,6 +40,27 @@ public class NO_1_ExampleCode {
         FolderInfoVO v3 = FolderInfoVO.createVOFORSearchParentFolder("FLD_123456789", "123456789", "123456789",
                                                                 "임시폴더", "임시폴더", "FLD_123456790");
 
+
+        // 반환 타입의 하위 타입 객체를 자유롭게 선택할 수 있다.
+        // Java의 Collection Framework가 예시
+
+        // : 인터페이스에 사용할 수 잇는 여러가지 Util Class로, Collections에서 얻도록 함.
+        Collections.addAll(null);
+        Collections.asLifoQueue(null);
+
+        // 이런 방식으로 45개의 Util을 사용할 수 있음.
+        // 동반 클래스(Companion Class) 방식을 사용하여 인스턴스화 불가인 클래스에게 클래스를 만들어 호출함.
+
+        // interface를 통해서 이 Util들을 공개하지 않고서도, 사용할 수 있다.
+        Collection collection = new ArrayList<>();
+        // 굳이 저 Util들의 기능을 이해할 필요 없이, CollectionFramework가 제공하는 ArrayList등의 구현체만을
+        // 사용할 줄 알면 된다.
+
+        // 입력 매개변수에 따라 각기 다른 클래스의 객체를 반환할 수 있다.
+        // 예시, Enum, 원소가 64개 이상이면 JumboEnumSet 제공한다.
+        // 즉 버전이 바뀐다 해도 EnumSet을 상속하는 객체를 던져주기만 하면 된다.
+        // ( 어차피 EnumSet의 기능을 할 것이므로 )
+        //EnumSet<int>.of(1,2,3)
     }
 
 }
