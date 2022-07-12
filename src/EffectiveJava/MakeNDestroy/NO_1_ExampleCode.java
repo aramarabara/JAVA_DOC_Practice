038401316581c215ec6cb570ecf290a19f73ca36
leeyoosong <zaxscd95@naver.com>
2022-07-12 20:55:23 +0900

    Java의 정석_정적팩토리메서드_7

--- src/EffectiveJava/MakeNDestroy/NO_1_ExampleCode.java
+++ src/EffectiveJava/MakeNDestroy/NO_1_ExampleCode.java
@@ -1,5 +1,10 @@
 package EffectiveJava.MakeNDestroy;
 
+import jdk.internal.reflect.Reflection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -7,7 +12,7 @@ import java.util.EnumSet;
 
 public class NO_1_ExampleCode {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
         // 현재 FbxFolderFileInfoVO를 만들기 위해서는 반드시
         // 이러한 값들이 들어가야 한다.
@@ -102,6 +107,56 @@ public class NO_1_ExampleCode {
         //
         //        }
 
+
+        // 작성하는 시점에서 반환할 객체의 클래스가 존재하지 않아도 된다.
+        // JDBC DriverManger의 경우 생성자를 private으로 막아놓았다.
+        //DriverManager manager = new DriverManager();
+
+        // JDBC DriverManager는 Connection 객체를 field로 가지고있지 않지만,
+        // 안의 메서드로 Connection을 받아 반환하는 것이 가능하다.
+        // 만약 생성자를 활용했다면, DriverManger와 Connection이 상속관계로 얽혀있어야 가능했을 것이다.
+
+        // getConnection이라는 static Factory Method가 reflection을 활용하여 Caller의 클래스를 알아내어서
+        // 반환한다.
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "root");
+
+        // Service Provider Framework? ( SPF? SPI? )
+        // 인터페이스 사용과 밀접한 연관이 있는 듯 한데..
+        // 그럼 Abstract Class처럼 인터페이스를 강제하지 않고서도 무조건 생성자로 생성하게 만드는건가?
+        // 즉, interface를 강제하지는 않지만 결국 instance화 하려면 인터페이스를 넣어주어야 하는..?
+        // 그건 아니다. interface가 초기값을 가지지 못하는 것과 달리
+        // Static Factory Method는 기본적으로 반환되는 Instance를  가질 수 있다.
+
+        // 결국 constructor 의 선언부에 강제되는 return type을 피해서 더 자유롭게 구성할 수 있다는 뜻
+
+        // 1. interface는 구현체가 있어야 한다..
+        // 그리고 Override를 강제한다. ( 기본값을 가지기 어려움 )
+        // implements로 관계를 만들어서 사용해야 한다.
+
+        // 2. Static Factory Method는 자기 자신을 통해 인스턴스를 구현한다.
+        // FactoryMethod이기 때문에 기본값을 가질 수 있다.
+        // 그리고, Reflection을 활용한다면 사용자가 새로 만든 전혀 모르는 타입도 활용할 수 있다.
+        // ( 생성자는 반환값이 지정되어 있어 불가능.. )
+
+        /*public static Connection getConnection(String url,
+                String user, String password) throws SQLException {
+            java.util.Properties info = new java.util.Properties();
+
+            if (user != null) {
+                info.put("user", user);
+            }
+            if (password != null) {
+                info.put("password", password);
+            }
+
+            // 이 부분에서 reflection을 활용하여 클래스 정보를 추출한다.
+            return (getConnection(url, info, Reflection.getCallerClass()));
+
+            // 보통 DriverClass는 드라이버마다 다 다른데, reflection을 통해서 현재 삽입된 클래스와
+            // 드라이버 간의 관계를 조명할 수 있다.
+            // 또한 이런 타입체크를 하면서 동시에
+
+        }*/
 
     }
 
