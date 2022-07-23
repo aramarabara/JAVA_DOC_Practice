45f5d074ea07bd273f140c470eb24d290a9d6bbd
leeyoosong <zaxscd95@naver.com>
2022-07-23 22:22:14 +0900

    EffectiveJava Package item5

--- src/EffectiveJava/CH1_ObjectMakeNDestroy/Item_2_useBuilder/A_ExampleCode.java
+++ src/EffectiveJava/CH1_ObjectMakeNDestroy/Item_2_useBuilder/A_ExampleCode.java
@@ -111,30 +111,64 @@ public class A_ExampleCode {
 
     }
 
-    public class NutritionFactsJavaBeanUseBuilderPattern {
+    public class NutritionFactsUseBuilderPattern {
 
-        private int servingSize = 0; // (제공량)
-        private int servings = 0; // (1회 제공랑)
-        private int calories = 0; // (칼로리)
-        private int fat = 0; // (지방)
-        private int sodium = 0; // (나트를 리스트에 추가하는 메서드
-        private int carbohydrate = 0; // (탄수화물)
+        private final int servingSize; // (제공량)
+        private final int servings; // (1회 제공랑)
+        private final int calories; // (칼로리)
+        private final int fat; // (지방)
+        private final int sodium; // (나트를 리스트에 추가하는 메서드
+        private final int carbohydrate; // (탄수화물)
 
-        public NutritionFactsJavaBeanUseBuilderPattern build() {
-            return new NutritionFactsJavaBeanUseBuilderPattern( this.servingSize, this.servings, this.calories, this.fat, this.sodium, this.carbohydrate);
+        public      class Builder {
+            //static, java 8 에서는 내부클래스에 Static 선언 불가.
+
+            private final int servingSize; // (제공량)
+            private final int servings; // (1회 제공랑)
+            private final int calories; // (칼로리)
+            private final int fat; // (지방)
+            private final int sodium; // (나트를 리스트에 추가하는 메서드
+            private final int carbohydrate; // (탄수화물)
+
+            public Builder(int servingSize, int servings, int calories, int fat, int sodium, int carbohydrate) {
+                this.servingSize = servingSize;
+                this.servings = servings;
+                this.calories = calories;
+                this.fat = fat;
+                this.sodium = sodium;
+                this.carbohydrate = carbohydrate;
+            }
+
+            public Builder calories(int calories) {
+                calories = calories;
+                return this;
+            }
+            public Builder fat(int fat) {
+                fat = fat;
+                return this;
+            }
+            public Builder sodium(int sodium) {
+                sodium = sodium;
+                return this;
+            }
+            public Builder carbohydrate(int carbohydrate) {
+                carbohydrate = carbohydrate;
+                return this;
+            }
+            public NutritionFactsUseBuilderPattern build() {
+                return new NutritionFactsUseBuilderPattern(this);
+            }
+
         }
 
-        public NutritionFactsJavaBeanUseBuilderPattern(int servingSize, int servings, int calories, int fat, int sodium, int carbohydrate) {
-            this.servingSize = servingSize;
-            this.servings = servings;
-            this.calories = calories;
-            this.fat = fat;
-            this.sodium = sodium;
-            this.carbohydrate = carbohydrate;
+        private NutritionFactsUseBuilderPattern(Builder builder) {
+            this.servingSize = builder.servingSize;
+            this.servings = builder.servings;
+            this.calories = builder.calories;
+            this.fat = builder.fat;
+            this.sodium = builder.sodium;
+            this.carbohydrate = builder.carbohydrate;
         }
-
-
-
     }
 
 
--- src/EffectiveJava/CH1_ObjectMakeNDestroy/Item_2_useBuilder/main.java
+++ src/EffectiveJava/CH1_ObjectMakeNDestroy/Item_2_useBuilder/main.java
@@ -4,11 +4,17 @@ public class main {
 
     public static void main(String[] args) {
 
+        // 1. 생성자로 호출하는 방법
+
         // Nutrition fact를 이렇게 호출하려면 필요없는 값은 0을 넣어주어야 한다.
         // 만일 매개변수가 10~20개를 넘는다면 엄청나게 많은 값들을 일일히 넣고, 가독성도 엄청나게 떨어진다.
         A_ExampleCode.NutritionFacts nutritionFacts = new A_ExampleCode.NutritionFacts(100, 0, 0, 0, 10, 10);
 
+
+        // 2. JavaBeans Pattern으로 호출하는 방법
+
         // 가독성이 더 좋아지고 모든 매개변수의 경우의 수에 관한 값을 넣어주지 않아도 되어 더 유연해졌다.
+        // 그러나, 객체가 생성된 이후 값이 들어가므로, 멀티쓰레드 환경에서는 객체의 불변성 (Conssistancy)을 보장하지 못한다.
         A_ExampleCode.NutritionFactsJavaBean nutritionFactsJavaBean = new A_ExampleCode.NutritionFactsJavaBean();
         nutritionFactsJavaBean.setServingSize(100);
         nutritionFactsJavaBean.setServings(0);
@@ -16,6 +22,15 @@ public class main {
         nutritionFactsJavaBean.setFat(0);
         nutritionFactsJavaBean.setSodium(10);
         nutritionFactsJavaBean.setCarbohydrate(10);
+
+
+        // 3. JavaBeans Pattern에 Freeze 기능을 추가하는 방법
+
+
+
+        // 3. BuilderPattern
+
+        // 멀티쓰레드 환경에서 객체 불변성을 보장한다. 필수 값들이 모두 Setting된 후 객체를 Build 하므로 값이 뒤바뀔 염려가 없다.
 
 
 
