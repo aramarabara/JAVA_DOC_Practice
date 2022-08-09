0258984e16b7587c6d5a4737a8d549508dd4d24d
leeyoosong <zaxscd95@naver.com>
2022-08-09 23:52:45 +0900

    Java Functional Programming

---
+++ src/JAVA_ORIGIN/Functional/FunctionalProgramming.java
@@ -0,0 +1,15 @@
+package JAVA_ORIGIN.Functional;
+
+public class FunctionalProgramming {
+
+    // Stream을 사용하기 위한 Functional Programming 학습
+
+    // 1. Java에서 functional Programming이란 java.util.function 패키지의 클래스들을 활용하는 것을 의미한다.
+
+    // 1) functional method라는 인터페이스를 구현한다.
+
+    Pradicate<T> predicate = (T t) -> {
+        return true;
+    };
+
+}
