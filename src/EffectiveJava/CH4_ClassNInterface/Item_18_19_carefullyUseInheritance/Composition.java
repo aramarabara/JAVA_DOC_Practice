2879f9b2f7ad31a40ddd42727ed70b4c4101f0ba
leeyoosong <zaxscd95@naver.com>
2022-08-29 23:52:00 +0900

    EFFECTIVE JAVA ITEM_18_19

---
+++ src/EffectiveJava/CH4_ClassNInterface/Item_18_19_carefullyUseInheritance/Composition.java
@@ -0,0 +1,15 @@
+package EffectiveJava.CH4_ClassNInterface.Item_18_19_carefullyUseInheritance;
+
+public class Composition {
+
+    public static void main(String[] args) {
+
+        //1. 상속은 캡슐화(Encapuslation)를 깨뜨린다.
+
+        // * 하위 클래스 확장성을 고려하지 않은 경우 지속적으로 변경이 발생한다.
+
+        // * 문서화가 제대로 되어있지 않은 경우 변경에 어려움을 겪는다.
+
+
+    }
+}
---
+++ src/EffectiveJava/CH4_ClassNInterface/Item_18_19_carefullyUseInheritance/Inheritance.java
@@ -0,0 +1,4 @@
+package EffectiveJava.CH4_ClassNInterface.Item_18_19_carefullyUseInheritance;
+
+public class Inheritance {
+}
