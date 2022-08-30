020813e467868af1dccc7d9db54514568e89e4b9
leeyoosong <zaxscd95@naver.com>
2022-08-30 23:55:58 +0900

    EFFECTIVE JAVA ITEM_18_19

--- src/EffectiveJava/CH4_ClassNInterface/Item_18_19_carefullyUseInheritance/Composition.java
+++ src/EffectiveJava/CH4_ClassNInterface/Item_18_19_carefullyUseInheritance/Composition.java
@@ -1,5 +1,7 @@
 package EffectiveJava.CH4_ClassNInterface.Item_18_19_carefullyUseInheritance;
 
+import java.util.List;
+
 public class Composition {
 
     public static void main(String[] args) {
@@ -10,6 +12,10 @@ public class Composition {
 
         // * 문서화가 제대로 되어있지 않은 경우 변경에 어려움을 겪는다.
 
+        // List.of
+        // >> 정적 팩터리 메서드, 불변객체를 생성하며 이름을 붙일 수 있어 가독성이 좋고, static이므로 성능개선에 도움이 된다 (비싼 객체)
+        List<?> listOf = List.of("1","2","3");
+        System.out.println(listOf.toString());
 
     }
 }
